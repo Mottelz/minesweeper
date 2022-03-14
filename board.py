83ffd6f360500c3bf4d0ff51417694c9792212f2
Mottelz <shachna@gmail.com>
2022-03-14 16:35:10 -0400

    Added comments to all functions

--- board.py
+++ board.py
@@ -4,7 +4,17 @@ NOT_CHECKED = -2
 
 
 class MinesweeperBoard:
+    '''
+    This class the represents a minesweeper board.
+    '''
+
     def __init__(self, width: int, height: int, mines: int):
+        '''
+        Create a new minesweeper board
+        :param width: the width of the board
+        :param height: the height of the board
+        :param mines: the number of mines on the board
+        '''
         self.__width = width
         self.__height = height
         self.__board = []
@@ -15,20 +25,32 @@ class MinesweeperBoard:
         self.__game_over = False
 
     def setup_board(self):
+        '''
+        Resets the board.
+        '''
+        # Reset attributes
         self.__game_over = False
         self.__flags = []
         self.__mines = []
         self.__board = []
 
+        # Pick spots for mines
         while len(self.__mines) < self.__mine_count:
             newMine = randint(0, self.__width * self.__height - 1)
             if newMine not in self.__mines:
                 self.__mines.append(newMine)
 
+        # FIll the board
         for i in range(self.__width * self.__height):
             self.__board.append(NOT_CHECKED if i not in self.__mines else MINE)
 
     def select(self, w: int, h: int):
+        '''
+        Select a square on the board
+        :param w: the X position of the cell
+        :param h: the Y position of the cell
+        :returns True if game continues, False if the game is over
+        '''
         square = self.convert_coord_to_board_position(w, h)
         if square in self.__flags:
             return True
@@ -44,6 +66,12 @@ class MinesweeperBoard:
             return True
 
     def flag(self, w, h):
+        '''
+        Flag a square on the board
+        :param w: the X position of the cell
+        :param h: the Y position of the cell
+        :returns Result of check_for_win()
+        '''
         square = self.convert_coord_to_board_position(w, h)
         if square not in self.__flags:
             self.__flags.append(square)
@@ -52,10 +80,17 @@ class MinesweeperBoard:
         return self.check_for_win()
 
     def check_for_win(self):
+        # TODO: You must complete this function so that it returns true if you won and false otherwise
         # If the list of mines and the list of flags matches, you win
         pass
 
     def count_surrounding_mines(self, w: int, h: int):
+        '''
+        Count mines surrounding a cell
+        :param w: the X position of the cell
+        :param h: the Y position of the cell
+        :returns Number of mines around the cell
+        '''
         spacesToCheck = []
         if w > 0:
             spacesToCheck.append(self.convert_coord_to_board_position(w - 1, h))
@@ -80,9 +115,11 @@ class MinesweeperBoard:
         return mine_count
 
     def convert_coord_to_board_position(self, w, h):
+        '''Converts 2D position to 1D position'''
         return w + h * self.__width
 
     def convert_square_to_char(self, cell):
+        '''Takes a 1D cell position and returns the char representing cell content'''
         square_value = self.__board[cell]
 
         if cell in self.__flags:
@@ -98,6 +135,7 @@ class MinesweeperBoard:
             return str(square_value)
 
     def __str__(self):
+        '''Returns board as a string'''
         out = 'X '
         for i in range(self.__width):
                 out += f"| {i} "
@@ -115,4 +153,8 @@ class MinesweeperBoard:
 
 if __name__ == "__main__":
     sample = MinesweeperBoard(5, 5, 10)
+    sample.select(0, 3)
+    sample.select(2, 3)
+    sample.select(0, 3)
+    sample.select(4, 3)
     print(sample)
